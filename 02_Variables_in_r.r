636a8a013da07fbe618395c095bb7c0cd7827d6f
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2023-09-16 19:11:05 +0545

    variable names

--- 02_Variables_in_r.r
+++ 02_Variables_in_r.r
@@ -21,7 +21,7 @@ num2 <- 10
 
 print(num1 + num2)
 
-# multiple vairables assignment one value 
+# multiple vairables assignment one value
 
 var1 <- var2 <- var3 <- "Orange"
 print(var1)
@@ -31,17 +31,11 @@ print(var3)
 
 
 
-# Variable Names 
-# A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for R variables are:
-# A variable name must start with a letter and can be a combination of letters, digits, period(.)
-# and underscore(_). If it starts with period(.), it cannot be followed by a digit.
+# Variable Names
+# A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume).  # nolint
+# Rules for R variables are:
+# A variable name must start with a letter and can be a combination of letters, digits, period(.) # nolint
+# and underscore(_). If it starts with period(.), it cannot be followed by a digit. # nolint
 # A variable name cannot start with a number or underscore (_)
-# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# Variable names are case-sensitive (age, Age and AGE are three different variables) # nolint
 # Reserved words cannot be used as variables (TRUE, FALSE, NULL, if...)
-
-
-
-
-
-
-
